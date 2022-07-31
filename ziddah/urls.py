5af6effbf7846237a52082791e4391bf342d2783
Aungshu <aungshu.shahu@gmail.com>
2022-08-01 00:58:48 +0600

    Url config changed

--- ziddah/urls.py
+++ ziddah/urls.py
@@ -18,14 +18,15 @@ from django.urls import path, include
 from finance import views
 
 from django.views.static import serve
-from django.config.urls import url
+
+#from django.config.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('finance.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+   # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+   # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
     #path('', views.add_show, name='add_show'),
     #path('aungshu', views.aungshu, name='aungshu')
